3894d60e2efbac0b01bd902b0febca8f960527b3
dZatr1k <uhuman239@gmail.com>
2023-03-23 19:14:53 +0300

    Update InputReader.cs

--- DungeonSlider/Assets/Scripts/InputReader.cs
+++ DungeonSlider/Assets/Scripts/InputReader.cs
@@ -21,13 +21,13 @@ public class InputReader : MonoBehaviour
     {
         if(_isBlocked == false)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) | Input.GetKeyDown(KeyCode.UpArrow))
                 OnWASDPressed?.Invoke(Vector2.up);
-            else if(Input.GetKeyDown(KeyCode.S))
+            else if(Input.GetKeyDown(KeyCode.S) | Input.GetKeyDown(KeyCode.DownArrow))
                 OnWASDPressed?.Invoke(Vector2.down);
-            else if (Input.GetKeyDown(KeyCode.A))
+            else if (Input.GetKeyDown(KeyCode.A) | Input.GetKeyDown(KeyCode.LeftArrow))
                 OnWASDPressed?.Invoke(Vector2.left);
-            else if (Input.GetKeyDown(KeyCode.D))
+            else if (Input.GetKeyDown(KeyCode.D) | Input.GetKeyDown(KeyCode.RightArrow))
                 OnWASDPressed?.Invoke(Vector2.right);
         }
     }
